18c21b3763646f05412a294e69871466ff2f1ec3
Auro <35087590+MrAuro@users.noreply.github.com>
2021-03-01 13:44:23 -0500

    Inital Commit

--- install.bat
+++
@@ -1,7 +0,0 @@
-set /p token=Bot Token:
-echo TOKEN=%token% > .env
-echo Made a .env file with your token.
-echo Installing node modules...
-npm install
-echo Node modules installed. Start run.bat to start the bot!
-pause
