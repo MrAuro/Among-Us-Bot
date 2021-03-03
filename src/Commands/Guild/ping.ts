0ba19258afed83aaa3800173b8f46c3a545457a2
Auro <35087590+MrAuro@users.noreply.github.com>
2021-03-02 23:34:08 -0500

    Added commands command and added a description field to other existing commands

--- src/Commands/Guild/ping.ts
+++ src/Commands/Guild/ping.ts
@@ -3,6 +3,7 @@ import { Command } from '../../Interfaces';
 export const command: Command = { 
     name: 'ping',
     aliases: ['p'],
+    description: 'Pings the bot for latency',
     run: async(client, message, args) => {
         message.channel.send("Pinging...").then((msg) => {
             const latency = msg.createdTimestamp - message.createdTimestamp;
